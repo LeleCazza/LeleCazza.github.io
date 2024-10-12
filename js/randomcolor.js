dc8b7bbe5507f69ebec109928eaf1bbc812dcbfa
Gabriele Cazzaniga <leye.lele@gmail.com>
2024-10-12 20:13:08 +0200

    added js menu, created modular footer and header

--- js/randomcolor.js
+++ js/randomcolor.js
@@ -3,12 +3,33 @@ function createColor(){
     var g = Math.floor((Math.random() * 127) + 129);
     var b = Math.floor((Math.random() * 127) + 129);
     return "rgb(" + r + "," + g + "," + b +")";
-}        
-    
-document.addEventListener("DOMContentLoaded",function() {
+}
+
+function openMenu() {
+    $("#menu").load("../menu.html");
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("overlay").style.display = "block"; 
     var color = createColor();
     var divs = document.querySelectorAll(".colore");
     divs.forEach(function (div) {
         div.style.background = color;
     })
-})
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
+    
+$(document.addEventListener("DOMContentLoaded",function() {
+    var color = createColor();
+    var divs = document.querySelectorAll(".colore");
+    divs.forEach(function (div) {
+        div.style.background = color;
+    })
+}))
+
+$(function(){
+    $("#footer").load("../footer.html");
+    $("#header").load("../header.html"); 
+});
