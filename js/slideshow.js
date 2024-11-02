d772905502a7ae835e296ac150fd092effdc73b9
Gabriele Cazzaniga <leye.lele@gmail.com>
2024-11-02 17:52:12 +0100

    update sistemi di numerazione

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,17 +1,16 @@
 var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivs(n, className) {
+  showDivs(slideIndex += n, className);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentDiv(n, className) {
+  showDivs(slideIndex = n, className);
 }
 
-function showDivs(n) {
+function showDivs(n, className) {
     var i;
-    var x = document.getElementsByClassName("slides");
+    var x = document.getElementsByClassName(className);
     if (n > x.length){
         slideIndex = 1
     }
@@ -24,11 +23,18 @@ function showDivs(n) {
     x[slideIndex-1].style.display = "block";  
 }
 
-function open_close(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
+function open_close(id, className) {
+  var x1 = document.getElementById(id);
+  var x2 = document.getElementsByClassName("w3-show");
+  for (let i = 0; i < x2.length; i++) {
+    if(x1 != x2[i])
+      x2[i].className = x2[i].className.replace(" w3-show", "");
   }
+  if (x1.className.indexOf("w3-show") == -1) {
+    x1.className += " w3-show";
+  } else { 
+    x1.className = x1.className.replace(" w3-show", "");
+  }
+  slideIndex = 1;
+  showDivs(slideIndex, className);
 }
