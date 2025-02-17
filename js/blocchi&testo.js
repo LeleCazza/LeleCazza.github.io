541d8c91ace0c78308bcd0f75e7239bfa96e955d
Gabriele Cazzaniga <gabriele.cazzaniga@ikmail.com>
2025-02-17 09:19:35 +0100

    suddivisione esercizi db e creazione presentazione estensioni base

--- js/blocchi&testo.js
+++ js/blocchi&testo.js
@@ -7,6 +7,15 @@ function InizioBlocco() {
     document.write(codiceHTML);
 }
 
+function InizioBloccoSlide() {
+    var codiceHTML = 
+    '<div class="w3-cell-row">'+
+    '   <div class="w3-container w3-cell w3-mobile">'+
+    '       <div class="w3-panel w3-card-4 colore w3-round-large">'+
+    '           <div class="w3-container w3-center" style="text-align: justify; ">'
+    document.write(codiceHTML);
+}
+
 function Testo(nomeEsercizio, pathEsercizio) {
     $("#" + nomeEsercizio).load(pathEsercizio);
 }
